d8405fd55c99a82321dba82d0839aead59461395
lexaimp <lexaimp1993rus@yandex.ru>
2018-09-23 21:53:10 +0700

    Доделал задачу про треугольник

--- lesson4/src/AreaTriangle.java
+++ lesson4/src/AreaTriangle.java
@@ -4,32 +4,37 @@ public class AreaTriangle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите координаты точек треугольника:");
-        System.out.print("x1 = ");
-        int x1 = scanner.nextInt();
-        System.out.print("y1 = ");
-        int y1 = scanner.nextInt();
-        System.out.print("x2 = ");
-        int x2 = scanner.nextInt();
-        System.out.print("y2 = ");
-        int y2 = scanner.nextInt();
-        System.out.print("x3 = ");
-        int x3 = scanner.nextInt();
-        System.out.print("y3 = ");
-        int y3 = scanner.nextInt();
 
-        if ((x1 == x2 && x1 == x3) || (y1 == y2 && y1 == y3)) {
+        System.out.print("x1 = ");
+        double x1 = scanner.nextInt();
+        System.out.print("y1 = ");
+        double y1 = scanner.nextInt();
+
+        System.out.print("x2 = ");
+        double x2 = scanner.nextInt();
+        System.out.print("y2 = ");
+        double y2 = scanner.nextInt();
+
+        System.out.print("x3 = ");
+        double x3 = scanner.nextInt();
+        System.out.print("y3 = ");
+        double y3 = scanner.nextInt();
+        double epsilon = 1.0e-10;
+
+        if (Math.abs((x1 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3)) - 0 <= epsilon) {
             System.out.println("Точки лежат на одной прямой");
         } else {
-            //Способ 1
+            //Способ 1 не из условия
             double area1 = Math.abs(0.5 * ((x1 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3)));
             System.out.println("1 сопсоб. Площадь: " + area1);
-            //Способ 2
-            //Нахождение длины сторон
+            //Способ 2 из условия
+            //Нахождение длин сторон
             double a = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
             double b = Math.sqrt(Math.pow(x3 - x1, 2) + Math.pow(y3 - y1, 2));
             double c = Math.sqrt(Math.pow(x3 - x2, 2) + Math.pow(y3 - y2, 2));
-            double p = (a + b + c) / 2;
-            double area2 = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+            double semiPerimeter = (a + b + c) / 2;
+            double area2 = Math.sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c));
             System.out.printf("2 способ. Площадь %.1f", area2);
         }
     }
