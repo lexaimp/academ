cab0f03df458e010a8f408cace012267dd63c89c
lexaimp <lexaimp1993rus@yandex.ru>
2018-10-07 21:54:39 +0700

    Макс подстрока, вроде сделал

---
+++ lesson9/src/it/java/academ/lesson9/CountingSymbol.java
@@ -0,0 +1,80 @@
+//import
+package it.java.academ.lesson9;
+
+public class CountingSymbol {
+    private String string;
+
+    public CountingSymbol(String string) {
+        this.string = string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public int getCountLitters() {
+        int count = 0;
+        for (int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i);
+            if (Character.isLetter(c)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int getCountDigit() {
+        int count = 0;
+        for (int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i);
+            if (Character.isDigit(c)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int getCountSpace() {
+        int count = 0;
+        for (int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i);
+            if (Character.isWhitespace(c)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int getCountOther() {
+        int count = 0;
+        for (int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i);
+            if (!Character.isWhitespace(c) && !Character.isLetter(c) && !Character.isDigit(c)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int getMaxSubstring() {
+        int count = 1;
+        int maxCount = 1;
+        char d = 0;
+        for (int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i);
+            if (Character.isUpperCase(c)){
+                c = Character.toLowerCase(c);
+            }
+            if (c == d){
+                count++;
+                if (maxCount <= count){
+                    maxCount = count;
+                }
+            } else {
+                count = 1;
+            }
+            d = c;
+        }
+        return maxCount;
+    }
+}
