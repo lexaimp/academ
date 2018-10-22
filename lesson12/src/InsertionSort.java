9994b0a8dfe0928c29c19d788917394ede226188
lexaimp <lexaimp1993rus@yandex.ru>
2018-10-22 22:54:48 +0700

    22.10.18 home

--- lesson12/src/InsertionSort.java
+++ lesson12/src/InsertionSort.java
@@ -1,21 +1,26 @@
+// import
+
 import java.util.Arrays;
 
 public class InsertionSort {
     public static void main(String[] args) {
-        int[] array = {5, 1, 35, 6, 97, 103, 17, 634, 42, 9};
+        int[] array = {5, 35, 1, 6, 97, 103, 17, 634, 42, 9};
         System.out.println(Arrays.toString(array));
         insertionSort(array);
         System.out.println(Arrays.toString(array));
     }
 
     private static void insertionSort(int[] array) {
-        for (int i = 1; i < array.length - 1; i++) {
-            for (int j = 0; j < i; j++) {
-                int temp = array[i];
-                if (temp < array[j]){
-                    array[i] = array[j];
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j;
+            for (j = i - 1; j >= 0; j--) {
+                if (temp >= array[j]) {
+                    break;
                 }
+                array[j + 1] = array[j];
             }
+            array[j + 1] = temp;
         }
     }
 }
