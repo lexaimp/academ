42686ef68e202136b6a9736e276948ac19c1471a
lexaimp <a.babushkin.rosdomofon.com>
2018-10-23 12:33:29 +0700

    23.10.18 work

--- lesson12/src/QuickSort.java
+++ lesson12/src/QuickSort.java
@@ -2,27 +2,28 @@ import java.util.Arrays;
 
 public class QuickSort {
     public static void main(String[] args) {
-        int[] array = {5, 35, 1, 6, 97, 103, 17, 634, 42, 9};
+        int[] array = {5, 35, 42, 2, 6, 103, 17, 634, 1, 9};
         System.out.println(Arrays.toString(array));
         quickSort(array, 0, array.length - 1);
         System.out.println(Arrays.toString(array));
     }
 
     private static void quickSort(int[] a, int left, int right) {
+        if (left == right){
+            return;
+        }
         int x = (a.length - 1) / 2;
-        m1:
-        for (int i = left; left < right; i++) {
+        for (int i = left; i < right; i++) {
             if (a[i] >= a[x]) {
-                for (int j = right; right > left; j--) {
-                    if (a[j] <= x) {
+                for (int j = right; j > left; j--) {
+                    if (a[j] <= a[x]) {
                         if (a[i] <= a[j]) {
                             int temp = a[i];
                             a[i] = a[j];
                             a[j] = temp;
+                        } else {
                             i++;
                             j++;
-                        } else {
-                            break m1;
                         }
                     }
                 }
