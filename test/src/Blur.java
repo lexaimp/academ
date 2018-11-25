b5b949adb939de838dde62b44b7bf31087bd31d4
lexaimp <lexaimp1993rus@yandex.ru>
2018-11-26 00:12:14 +0700

    26.11.18 home

--- test/src/Blur.java
+++ test/src/Blur.java
@@ -5,17 +5,17 @@ import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Blur {
 
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите размер матрицы");
+        // создание переменных для хранения размера матрицы
+        int matrixWidth = 3;
+        int matrixHeight = 3;
         // создание матрицы для хранения необходимого эффекта
-        double[][] matrixBlur = getMatrix(scanner.nextInt(), scanner.nextInt());
+        double[][] matrixBlur = getMatrix(matrixWidth, matrixHeight);
 
-        // читаем картинку из файлу image.jpg в объект класса BufferedImage
+        // читаем картинку из файла image.jpg в объект класса BufferedImage
         BufferedImage image = ImageIO.read(new File("image.jpg"));
         WritableRaster raster = image.getRaster();
 
@@ -24,23 +24,28 @@ public class Blur {
         int height = raster.getHeight();
 
         final int COLORS_COUNT_IN_RGB = 3;
+        // Создание пустой картинки такоко же размера что и искомая
         BufferedImage imageCopy = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         WritableRaster rasterCopy = imageCopy.getRaster();
         // создаем массив, в котором будет содержаться текущий пиксель
         int[] pixel = new int[COLORS_COUNT_IN_RGB];
+        // количестов пикселей от искомого
+        int countElementsOfWidthDesiredPixel = matrixWidth / 2;
+        int countElementsOfHeightDesiredPixel = matrixHeight / 2;
+
         // цикл по строкам картинки
-        for (int j = 1; j < height - 1; ++j) {
+        for (int j = countElementsOfHeightDesiredPixel; j < height - countElementsOfHeightDesiredPixel; ++j) {
             // цикл пикселям строки
-            for (int i = 1; i < width - 1; ++i) {
+            for (int i = countElementsOfWidthDesiredPixel; i < width - countElementsOfWidthDesiredPixel; ++i) {
 
                 double r = 0;
                 double g = 0;
                 double b = 0;
 
                 int index1 = 0;
-                for (int m = j - 1; m <= j + 1; ++m) {
+                for (int m = j - countElementsOfHeightDesiredPixel; m <= j + countElementsOfHeightDesiredPixel; ++m) {
                     int index2 = 0;
-                    for (int n = i - 1; n <= i + 1; ++n) {
+                    for (int n = i - countElementsOfWidthDesiredPixel; n <= i + countElementsOfWidthDesiredPixel; ++n) {
                         raster.getPixel(n, m, pixel);
 
                         r += matrixBlur[index1][index2] * pixel[0];
@@ -54,11 +59,11 @@ public class Blur {
                 pixel[1] = getSat(g);
                 pixel[2] = getSat(b);
 
-                raster.setPixel(i, j, pixel);
+                rasterCopy.setPixel(i, j, pixel);
             }
         }
 
-        ImageIO.write(image, "png", new File("out.png"));
+        ImageIO.write(imageCopy, "png", new File("out.png"));
     }
 
     private static double[][] getMatrix(int matrixWidth, int matrixHeight) {
